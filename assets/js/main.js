f1646e1d5897cc07ddcb7d4b83b7fd0f2525c53f
Daniel <d.c.Poolman@student.tudelft.nl>
2021-12-10 22:07:39 +0100

    reformat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
-$(function() {
+$(function () {
 
     "use strict";
 
     //===== Prealoder
 
-    $(window).on('load', function(event) {
+    $(window).on('load', function (event) {
         $('.preloader').delay(500).fadeOut(500);
     });
 
 
     //===== Sticky
 
-    $(window).on('scroll', function(event) {
+    $(window).on('scroll', function (event) {
         var scroll = $(window).scrollTop();
         if (scroll < 10) {
             $(".navbar-area").removeClass("sticky");
-        } else{
+        } else {
             $(".navbar-area").addClass("sticky");
         }
     });
@@ -34,10 +34,10 @@ $(function() {
     //     // Active link switching
     //     $(window).scroll(function() {
     //     var scrollbarLocation = $(this).scrollTop();
-		//
+    //
     //     scrollLink.each(function() {
     //       var sectionOffset = $(this.hash).offset().top - 73;
-		//
+    //
     //       if ( sectionOffset <= scrollbarLocation ) {
     //         $(this).parent().addClass('active');
     //         $(this).parent().siblings().removeClass('active');
@@ -48,20 +48,20 @@ $(function() {
 
     //===== Mobile Menu
 
-    $(".navbar-toggler").on('click', function(){
+    $(".navbar-toggler").on('click', function () {
         $(this).toggleClass("active");
     });
 
-    $(".navbar-nav a").on('click', function() {
+    $(".navbar-nav a").on('click', function () {
         $(".navbar-toggler").removeClass('active');
     });
 
 
     //===== Countdown
 
-    $('[data-countdown]').each(function() {
+    $('[data-countdown]').each(function () {
         var $this = $(this), finalDate = $(this).data('countdown');
-            $this.countdown(finalDate, function(event) {
+        $this.countdown(finalDate, function (event) {
             $this.html(event.strftime('<div class="header-countdown pt-70 d-flex justify-content-center"><div class="single-count-content count-color-1"><span class="count">%D</span><p class="text">Days</p></div><div class="single-count-content count-color-2"><span class="count">%H</span><p class="text">Hours</p></div><div class="single-count-content count-color-3"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content count-color-4"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
         });
     });
@@ -75,10 +75,10 @@ $(function() {
     //===== Magnific Popup
 
     $('.image-popup').magnificPopup({
-      type: 'image',
-      gallery:{
-        enabled:true
-      }
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
 
 
@@ -100,47 +100,47 @@ $(function() {
         slidesToScroll: 1,
         responsive: [
             {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 3,
-              }
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
             },
             {
-              breakpoint: 992,
-              settings: {
-                slidesToShow: 2,
-              }
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
             },
             {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-              }
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
             },
             {
-              breakpoint: 576,
-              settings: {
-                slidesToShow: 1,
-              }
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
             }
         ]
-        });
+    });
 
 
     //===== Back to top
 
     // Show or hide the sticky footer button
-    $(window).on('scroll', function(event) {
-        if($(this).scrollTop() > 600){
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
             $('.back-to-top').fadeIn(200)
-        } else{
+        } else {
             $('.back-to-top').fadeOut(200)
         }
     });
 
 
     //Animate the scroll to yop
-    $('.back-to-top').on('click', function(event) {
+    $('.back-to-top').on('click', function (event) {
         event.preventDefault();
 
         $('html, body').animate({
@@ -150,20 +150,6 @@ $(function() {
 
 
     //=====
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
